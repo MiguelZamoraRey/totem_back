6ff9a52581533016543eedfc010d6d65bba738fc
Miguel <miguelzamorarey@gmail.com>
2018-07-19 11:28:21 +0200

    Add files via upload

--- services/jwt.js
+++ services/jwt.js
@@ -12,11 +12,8 @@ exports.createToken = function(user){
     var payload = {
         sub: user._id,
         name: user.name,
-        surname: user.surname,
-        nick: user.nick,
         email: user.email,
-        role: user.role,
-        image: user.image,
+        avatar: user.avatar,
         iat: moment().unix(),
         exp: moment().add(30, 'days').unix()
     };
